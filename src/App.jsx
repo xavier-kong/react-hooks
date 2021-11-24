316c78620ba9d9ee39b1b0056fc7fe610c83bcc4
xavier-kong <kongxianchengxavier@gmail.com>
2021-11-24 18:25:08 +0800

    added up to State Hook example at Hooks at a Glance

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from 'react';
 
-export default () => (
-  <>
-    <h1>Welcome to React Vite Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+function App() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+export default App;
